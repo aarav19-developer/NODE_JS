9d18d736275944f2536847c8bc5c47c6e4860c90
Aarav Harit <aaravharit418@gmail.com>
2026-08-10 21:28:32 +0530

    Class_Journey

---
+++ index1.js
@@ -0,0 +1,41 @@
+console.log("Start");
+
+setTimeout(() => {
+    console.log(one())
+}, 3000);
+
+console.log("Mid")
+
+function one() {
+    console.log(name);
+
+    var name = AARAVS
+    console.log(name);
+
+    three()
+}
+
+setTimeout(() => {
+    two()
+}, 1000);
+
+function two(){
+    console.log("error")
+
+    let id = 22
+    console.log(id);
+
+    console.log(three())
+
+    return 2
+}
+
+function three() {
+    return "Hello from S"
+}
+
+setTimeout(() => {
+    console.log(three())
+}, 10000);
+
+console.log("End")
