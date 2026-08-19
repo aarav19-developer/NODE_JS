f197a367c0b5faf22b1780cd084e5527f826cc24
Aarav Harit <aaravharit418@gmail.com>
2026-08-19 13:42:13 +0530

    Class_Journey

---
+++ index7/index7.js
@@ -0,0 +1,10 @@
+// Step 1:  npm init -y [setup nodejs app]
+// Step 2: npm i is-even [installing is-even external module]
+// Step 3: import is-even module for usecase
+
+const isEven = require("Is-even")
+
+// Step 4: use
+
+console.log(isEven(1))
+console.log(isEven(2))
